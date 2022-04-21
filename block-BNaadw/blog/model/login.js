2b6dd094b90809eef383a7ee0b61232731fbdcf9
Vijay Gupta <vijayg963@gmail.com>
2022-04-21 23:39:27 +0530

    Assignment-2

---
+++ block-BNaadw/blog/model/login.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var bcrypt = require('bcrypt');
+
+var userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, minlength: 5, required: true },
+    age: { type: Number, default: 0 },
+    phone: { type: Number, minlength: 10 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', function (next) {
+  if (this.password && this.isModified('password')) {
+    bcrypt.hash(this.password, 10, (err, hashed) => {
+      if (err) return next(err);
+      this.password = hashed;
+      return next();
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.verifyPassword = function (password, cb) {
+  bcrypt.compare(password, this.password, (err, result) => {
+    return cb(err, result);
+  });
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ block-BNaadw/blog/routes/users.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var router = express.Router();
+
+var User = require('../model/user-login');
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  console.log(req.session);
+  res.render('users');
+});
+
+// login form
+router.get('/login', function (req, res, next) {
+  var error = req.flash('error')[0];
+  console.log(error);
+  res.render('login', { error });
+});
+
+router.get('/registration', function (req, res, next) {
+  res.render('registration', { error: req.flash('error')[0] });
+});
+
+router.post('/registration', function (req, res, next) {
+  User.create(req.body, (err, user) => {
+    if (err) {
+      if (err.name === 'MongoError') {
+        req.flash('error', 'This email is taken');
+        return res.redirect('/users/registration');
+      }
+      if (err.name === 'validationError ') {
+        req.flash('error', err.message);
+        return res.redirect('/users/register');
+      }
+      return res.json({ err });
+    }
+    res.redirect('/users/login');
+  });
+});
+
+router.post('/login', function (req, res, next) {
+  var { email, password } = req.body;
+  console.log(email, password);
+  if (!email || !password) {
+    req.flash('error', 'Email/Password required');
+    return res.redirect('/users/login');
+  }
+  User.findOne({ email }, (err, user) => {
+    if (err) return next(err);
+    // no user
+    if (!user) {
+      req.flash('error', 'This email is not registered');
+      return res.redirect('/users/login');
+    }
+    // campare password
+    user.verifyPassword(password, (err, result) => {
+      console.log(err, result);
+      if (err) return next(err);
+      if (!result) {
+        req.flash('error', 'Incorrect password');
+        return res.redirect('/users/login');
+      }
+      // parsist logged in user information
+      req.session.userId = user.id;
+      res.redirect('/users');
+    });
+    0;
+  });
+});
+
+router.get('/logout', (req, res) => {
+  req.session.destroy();
+  // res.clearCookie('connect.sid');
+  res.redirect('/users/login');
+});
+
+module.exports = router;
